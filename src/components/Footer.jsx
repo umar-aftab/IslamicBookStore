8f573739d52ba47d23b1685b55959969aca3e372
umar-aftab <umaraftabqamar@gmail.com>
2024-09-17 09:27:07 -0600

    Added Header elements and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function Footer(){
     return(
-        <></>
+        <footer class="bg-orange-950 text-white py-4 mt-2">
+            <div class="container mx-auto text-center">
+                <p>Build your library with knowledge.</p>
+            </div>
+            <div class="mt-8 font-extralight">
+                <p>&copy;2024 SunnahApps. All Rights Reserved.</p>
+            </div>
+        </footer>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,22 @@ import React from "react";
 function Header(){
     return (
         <nav class="bg-primary p-4 flex justify-between items-center bg-orange-950">
-         
+            <div class="flex items-center space-x-2">
+                <img src={`${process.env.PUBLIC_URL}/img/logo.png`} class="h-20 w-20"/>
+
+            </div>
+
+            <ul class="flex space-x-6 text-white text-center">
+                <li><a href="#" class="hover:text-yellow-600">Home</a></li>
+                <li><a href="#" class="hover:text-yellow-600">About Us</a></li>
+                <li><a href="#" class="hover:text-yellow-600">Books</a></li>
+                <li><a href="#" class="hover:text-yellow-600">Contact</a></li>
+            </ul>
+
+            <div class="flex space-x-4">
+                <button class="bg-orange-500 rounded text-white px-3 py-3">Cart</button>
+                <button class="bg-orange-500 rounded text-white px-3 py-3">Login</button>
+            </div>
         </nav>
     );
 }
